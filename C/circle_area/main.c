732ffbc9bdf6e574508254f8bcb21be79a6a6202
weversonbarbieri <weverson.barbieri@gmail.com>
2023-10-31 17:39:30 -0400

    Cyrcle's area
    Type the cyrcle's radius and calculate the  Cyrcle's area.

---
+++ C/circle_area/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+
+   double PI = 3.14159, radius, area;
+
+   printf("Type the value of the Cyrcle's radius: ");
+   scanf("%lf", &radius);
+
+   area = PI * (pow(radius, 2));
+
+   printf("Cyrcle area = %.3lf\n", area);
+
+
+
+
+    return 0;
+}
